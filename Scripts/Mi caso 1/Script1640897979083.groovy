02fdf0db29b8ee480740449d3ea0224a16873ec8
Ronaldo <Ronaldo@ASUS-PC>
2021-12-30 16:02:04 -0500

    ok

---
+++ Scripts/Mi caso 1/Script1640897979083.groovy
@@ -0,0 +1,39 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://demoqa.com/')
+
+WebUI.click(findTestObject('Object Repository/Mi caso 1/Page_ToolsQA/div_Elements_card-up'))
+
+WebUI.click(findTestObject('Object Repository/Mi caso 1/Page_ToolsQA/span_Text Box'))
+
+WebUI.setText(findTestObject('Object Repository/Mi caso 1/Page_ToolsQA/input_Full Name_userName'), 'Ronaldo Rosales')
+
+WebUI.setText(findTestObject('Object Repository/Mi caso 1/Page_ToolsQA/input_Email_userEmail'), 'ronaldo@live.com')
+
+WebUI.setText(findTestObject('Object Repository/Mi caso 1/Page_ToolsQA/textarea_Current Address_currentAddress'), 'Quito')
+
+WebUI.setText(findTestObject('Object Repository/Mi caso 1/Page_ToolsQA/textarea_Permanent Address_permanentAddress'), 'Quito')
+
+WebUI.click(findTestObject('Object Repository/Mi caso 1/Page_ToolsQA/button_Submit'))
+
+WebUI.closeBrowser()
+
